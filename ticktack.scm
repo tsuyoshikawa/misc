398ec284d8e88878b2e36bc8ca50778af094b812
Tsuyoshi Yoshikawa <tsuyoshikawa@gmail.com>
2009-06-15 23:46:49 +0900

    basic functions are readygit add ticktack.scm

--- ticktack.scm
+++ ticktack.scm
@@ -1,93 +1,111 @@
 (use srfi-27)
 
+;;乱数の種リセット
 (random-source-randomize! default-random-source)
 
-;;リストにある要素が含まれるか
+;;リストにある要素が含まれるか #t/#f
 (define (include? needle heystack)
   (if (null? heystack)
       #f
       (if (equal? needle (car heystack))
-          #t
-          (include? needle (cdr heystack))
-          )
+	  #t
+	  (include? needle (cdr heystack))
+	  )
       )
   )
 
-;;リストのサイズを調べる
-(define (size-of list)
-  (fold (lambda (a b) (+ b 1)) 0 list)
-  )
-
-;;再帰バージョン
-(define (size-of list)
-  (if (null? list)
+;;リストのサイズを測る
+(define (size-of lis)
+  (if (null? lis)
       0
-      (+ 1 (size-of (cdr list)))
+      (+ 1 (size-of (cdr lis)))
       )
   )
 
 (print (size-of '(1 2 3 4 5))) ;=> 5
 
 ;;n番目の要素を取り出す
-(define (value-at list n)
-  (if (or (null? list) (< n 0) (< (size-of list) n))
+(define (value-at lis n)
+  (if (or (null? lis) (< n 0) (< (size-of lis) n))
       '()
       (if (equal? n 1)
-          (car list)
-          (value-at (cdr list) (- n 1))
-          )
+	  (car lis)
+	  (value-at (cdr lis) (- n 1))
+	  )
       )
   )
 
 (print (value-at '(1 2 6 9) 4)) ;=> 9
 
-;;要素の追加(空リストなら無視され、アトムなら連結してリストにされる)
-(define (force-merge from to)
-  (if (and (pair? from) (pair? to))
-      (cons (car from) (force-merge (cdr from) to))
-      (if (null? from)
-          to
-          (if (pair? to)
-              (cons from to)
-              (list from to)
-              )
-          )
+;;１リストに１リストを付け加える
+(define (my-append a b)
+  (if (pair? a)
+      (cons (car a) (my-append (cdr a) b))
+      b
       )
   )
 
-(print (force-merge 1 3)) ;=> (1 3)
-(print (force-merge '(1 3) 5)) ;=> (1 3)
-(print (force-merge 1 '(3 4))) ;=> (1 3 4)
-(print (force-merge '() '(3 4))) ;=> (3 4)
-(print (force-merge '(1 2) '(3 4))) ;=> (1 2 3 4)
+(print (my-append '(1 2 3) '(4 5 6))) ;=> (1 2 3 4 5 6)
+
+;;必ず平らなリストにして返す
+(define (flatten lis)
+  (if (null? lis)
+      '()
+      (if (pair? lis)
+	  (my-append (flatten (car lis)) (flatten (cdr lis)))
+	  (list lis)
+	  )
+      )
+  )
+
+;;強制的に２つのリストをネスト無しのリストとして結合する
+(define (non-nested-merge a b)
+  (my-append (flatten a) (flatten b))
+  )
+
+(print (non-nested-merge 1 3)) ;=> (1 3)
+(print (non-nested-merge '(1 3) 5)) ;=> (1 3 5)
+(print (non-nested-merge 1 '(3 4))) ;=> (1 3 4)
+(print (non-nested-merge '() '(3 4))) ;=> (3 4)
+(print (non-nested-merge '(1 2) '(3 4))) ;=> (1 2 3 4)
 
 ;;要素の削除
-(define (delete-at list n)
+(define (delete-at lis n)
+  (if (null? lis)
+      '()
+      (if (pair? lis)
+	  (if (equal? (car lis) n)
+	      (delete-at (cdr lis) n)
+	      (cons (car lis) (delete-at (cdr lis) n))
+	      )
+	  )
+      )
   )
+
+(print (delete-at '(1 2 (3 1) 4) '(3 1)))
 
 ;;リストからランダムに一つ取り出す
-(define (random-one list)
-  (value-at list (+ 1 (random-integer (- (size-of list) 1))))
+(define (random-one lis)
+  (value-at lis (+ 1 (random-integer (- (size-of lis) 1))))
   )
 
 (print (random-one '(10 20 30 40 50)))
 (print (random-one '(10 20 30 40 50)))
 (print (random-one '(10 20 30 40 50)))
-
-
 
 ;;勝利判定
 (define (win? player)
   (include? (sort player) '((1 2 3) (4 5 6) (7 8 9) (1 4 7) (2 5 8) (3 6 9) (1 5 9) (3 5 7)))
   )
 
-(if (win? '(1 2 3))
-    (print "win")
-    (print "loose")
-    ) ;=>"win"
+(if (win? '(1 2 3)) (print "win") (print "loose")) ;=>"win"
 
-#;;盤から一つ番号を選ぶ
+;;盤から一つ番号を選ぶ
 (define (choice player board)
-  (begin (push player choiced) (delete board choice))
+  (let ((choiced (random-one board)))
+    (values #?=(non-nested-merge player choiced) #?=(delete-at board choiced))
+    )
   )
+
+(choice '() '(1 2 3 4 5 6 7 8 9))
 
