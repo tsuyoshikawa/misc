a29b7249b70b483049c8fd6e39bdf219d47ebcae
Tsuyoshi Yoshikawa <tsuyoshikawa@gmail.com>
2009-06-16 18:47:16 +0900

    maru-batu game for Scheme

--- ticktack.scm
+++ ticktack.scm
@@ -1,16 +1,34 @@
+;;ランダムビットのソースモジュール
 (use srfi-27)
 
-;;乱数の種リセット
+;;乱数の種をセット。これやらないと、random-integerが毎回一緒の値を返す
 (random-source-randomize! default-random-source)
 
-;;リストにある要素が含まれるか #t/#f
+;;リストにある要素が含まれるか
 (define (include? needle heystack)
   (if (null? heystack)
       #f
       (if (equal? needle (car heystack))
-	  #t
-	  (include? needle (cdr heystack))
-	  )
+          #t
+          (include? needle (cdr heystack))
+          )
+      )
+  )
+
+;;２つのリストの積集合を返す
+(define (intersection a b)
+  (if (or (null? a) (null? b))
+      '()
+      (if (pair? a)
+          (if (include? (car a) b)
+              (cons (car a) (intersection (cdr a) b))
+              (intersection (cdr a) b)
+              )
+          (if (include? a b)
+              a
+              '()
+              )
+          )
       )
   )
 
@@ -22,20 +40,16 @@
       )
   )
 
-(print (size-of '(1 2 3 4 5))) ;=> 5
-
 ;;n番目の要素を取り出す
 (define (value-at lis n)
   (if (or (null? lis) (< n 0) (< (size-of lis) n))
       '()
       (if (equal? n 1)
-	  (car lis)
-	  (value-at (cdr lis) (- n 1))
-	  )
+          (car lis)
+          (value-at (cdr lis) (- n 1))
+          )
       )
   )
-
-(print (value-at '(1 2 6 9) 4)) ;=> 9
 
 ;;１リストに１リストを付け加える
 (define (my-append a b)
@@ -45,16 +59,14 @@
       )
   )
 
-(print (my-append '(1 2 3) '(4 5 6))) ;=> (1 2 3 4 5 6)
-
 ;;必ず平らなリストにして返す
 (define (flatten lis)
   (if (null? lis)
       '()
       (if (pair? lis)
-	  (my-append (flatten (car lis)) (flatten (cdr lis)))
-	  (list lis)
-	  )
+          (my-append (flatten (car lis)) (flatten (cdr lis)))
+          (list lis)
+          )
       )
   )
 
@@ -63,61 +75,90 @@
   (my-append (flatten a) (flatten b))
   )
 
-(print (non-nested-merge 1 3)) ;=> (1 3)
-(print (non-nested-merge '(1 3) 5)) ;=> (1 3 5)
-(print (non-nested-merge 1 '(3 4))) ;=> (1 3 4)
-(print (non-nested-merge '() '(3 4))) ;=> (3 4)
-(print (non-nested-merge '(1 2) '(3 4))) ;=> (1 2 3 4)
-
 ;;要素の削除
 (define (delete-at lis n)
   (if (null? lis)
       '()
       (if (pair? lis)
-	  (if (equal? (car lis) n)
-	      (delete-at (cdr lis) n)
-	      (cons (car lis) (delete-at (cdr lis) n))
-	      )
-	  )
+          (if (equal? (car lis) n)
+              (delete-at (cdr lis) n)
+              (cons (car lis) (delete-at (cdr lis) n))
+              )
+          )
       )
   )
 
-(print (delete-at '(1 2 (3 1) 4) '(3 1)))
-
 ;;リストからランダムに一つ取り出す
 (define (random-one lis)
-  (value-at lis (+ 1 (random-integer (- (size-of lis) 1))))
+  (value-at lis (+ 1 (random-integer (size-of lis))))
   )
 
-(print (random-one '(10 20 30 40 50)))
-(print (random-one '(10 20 30 40 50)))
-(print (random-one '(10 20 30 40 50)))
+;;2番目の要素を（アトムとして）取り出す。
+(define (2nd lis)
+  (if (or (null? lis) (not (pair? lis)))
+      '()
+      (car (cdr lis))
+      )
+  )
+
+;;勝利パターン
+(define win-patterns '((1 2 3) (4 5 6) (7 8 9) (1 4 7) (2 5 8) (3 6 9) (1 5 9) (3 5 7)))
 
 ;;勝利判定
 (define (win? player)
-  (include? (sort player) '((1 2 3) (4 5 6) (7 8 9) (1 4 7) (2 5 8) (3 6 9) (1 5 9) (3 5 7)))
+  (include?
+   #t
+   (map
+    (lambda (pattern)
+      (equal? (size-of (intersection player pattern)) 3)
+      )
+    win-patterns)
+   )
   )
-
-(if (win? '(1 2 3)) (print "win") (print "loose")) ;=>"win"
 
 ;;盤から一つ番号を選ぶ
 (define (choice player board)
   (let ((choiced (random-one board)))
-    (begin (non-nested-merge player choiced) (delete-at board choiced))
+    (values
+     (non-nested-merge player choiced)
+     (delete-at board choiced)
+     )
     )
   )
 
-(choice '() '(1 2 3 4 5 6 7 8 9))
-
-(define (game)
-  (map
-   (lambda (player)
-     (if (or (null? board) (win? player))
-	 (print "win!")
-	 (choice player board)
-	 )
-     )
-   '(() ())
-   )
+;;決着するまで回数を重ねる
+(define (fight board p1 p2)
+  (if (null? board)
+      (cond [(win? (2nd p1)) (car p1)]
+            [(win? (2nd p2)) (car p2)]
+            [else 'draw]
+            )
+      (receive (pl bo)
+               (choice (2nd p1) board)
+               (if (win? pl)
+                   (car p1)
+                   (fight bo p2 (cons (car p1) (list pl)))
+                   )
+               )
+      )
   )
 
+;;1ゲーム実行
+(define (1game)
+  (fight '(1 2 3 4 5 6 7 8 9) '(P1 ()) '(P2 ()))
+  )
+
+;;指定回数ゲーム実行
+(define (run c1 c2 cd max)
+  (if (equal? (+ c1 c2 cd) max)
+      (print "P1:" c1 " " "P2:" c2 " " "draw:" cd)
+      (case (1game)
+        ((P1) (run (+ c1 1) c2 cd max))
+        ((P2) (run c1 (+ c2 1) cd max))
+        (else (run c1 c2 (+ cd 1) max))
+        )
+      )
+  )
+
+;;実行
+(run 0 0 0 10000)
